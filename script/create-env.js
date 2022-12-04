c96e3d9ae58f127e84aaf08f54f783fa379034d1
Didier Contreras <didier9133@gmail.com>
2022-12-03 21:20:46 -0400

    [ADD] environments secrets

--- script/create-env.js
+++
@@ -1,4 +0,0 @@
-const fs=require('fs')
-
-fs.writeFileSync('./.env', `REACT_APP_CLIENT_ID=${process.env.REACT_APP_CLIENT_ID}\n`)
-fs.writeFileSync('./.env',`REACT_APP_GOOGLE_KEY=${process.env.REACT_APP_GOOGLE_KEY}\n` )
